b632986e1f5c899410148c92818cc8d7aff0659f
S4W4A <matteo.montanari@erstegroup.com>
2024-04-22 00:08:43 +0200

    many changes

---
+++ src/components/ui/navigation.tsx
@@ -0,0 +1,47 @@
+
+import { Popover } from '@headlessui/react'
+
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+
+export default function Navigation() {
+  
+
+  return ( 
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+ 
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+              Product
+              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            </Popover.Button>
+
+          </Popover>
+
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Features
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Marketplace
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Company
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+ 
+  )
+}
