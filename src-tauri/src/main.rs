177dc604a9592d8526ea398d972090ca63369b18
S4W4A <matteo.montanari@erstegroup.com>
2024-06-10 22:41:04 +0200

    remote

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
