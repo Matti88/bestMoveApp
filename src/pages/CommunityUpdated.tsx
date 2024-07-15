90b983e8c2aceefc6bf69e872f5e62e9159ce33c
S4W4A <matteo.montanari@erstegroup.com>
2024-07-15 22:47:02 +0200

    new items

--- src/pages/CommunityUpdated.tsx
+++ src/pages/CommunityUpdated.tsx
@@ -13,21 +13,21 @@ const cards_houses_proposed = [
     description: "A sample of 2000 houses in Vienna that have been proposed for sale on the platform Willhabe.at",
     substitle1: "Prices ranging from 200K€ to 400K€",
     substitle2: "Areas from 50m2 to 100m2",
-    pathToFileURL: "/houses.json",
+    pathToFileURL: "/houses_buy.json",
   },
   {
     title: "Apartments for Rent in Vienna",
     description: "A sample of 2000 apartments in Vienna that have been proposed for rent on the platform Willhabe.at",
     substitle1: "Prices ranging from 1000€ to 2000€",
     substitle2: "Areas from 50m2 to 100m2",
-    pathToFileURL: "/houses.json",
+    pathToFileURL: "/apartmnt_rent.json",
   },
   {
     title: "Houses for Rent in Vienna",
     description: "A sample of 2000 apartments in Vienna that have been proposed for sale on the platform Willhabe.at",
     substitle1: "Prices ranging from 1000€ to 2000€",
     substitle2: "Areas from 50m2 to 100m2",
-    pathToFileURL: "/houses.json",
+    pathToFileURL: "/houses_rent.json",
   },
 ]
 
