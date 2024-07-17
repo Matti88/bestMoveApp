0e9f92779746c92fb3f351e19141cd4444f19034
S4W4A <matteo.montanari@erstegroup.com>
2024-07-17 23:33:26 +0200

    first mobile adjustments

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,12 +1,18 @@
 import { appConfig } from "@/config/app";
 import { ModeToggle } from "../mode-toggle";
+import { PoiDrawer } from '@/components/ui/PoiDrawer';
+import  FormComponentFooter  from '@/components/ui/FormComponentFooter';
 
 export function Footer() {
     return (
         <footer className="flex flex-col items-center justify-between gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row ml-20 mr-20" >
-            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.</p>
+            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left hidden md:block">Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.</p>
             <div className="hidden md:block">
                 <ModeToggle />
+            </div>
+            <div className="flex flex-row items-center gap-4 sm:hidden">
+                <FormComponentFooter />
+                <PoiDrawer />
             </div>
         </footer>
     )
---
+++ src/components/ui/PoiDrawer.tsx
@@ -0,0 +1,94 @@
+import * as React from "react"
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons"
+import { Bar, BarChart, ResponsiveContainer } from "recharts"
+import POIList from '@/components/ui/POIList';
+
+import { Button } from "@/components/ui/shadcn/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/shadcn/drawer"
+
+const data = [
+  {
+    goal: 400,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 239,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 349,
+  },
+]
+
+export function PoiDrawer() {
+  const [goal, setGoal] = React.useState(350)
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)))
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="secondary">POIs</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm p-4">
+          <DrawerHeader>
+            <DrawerTitle>Point of Interest</DrawerTitle>
+            <DrawerDescription>List of POIs, change or delete them</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+ 
+            <div className="mt-3 h-[120px]">
+            <POIList/>
+            </div>
+          </div>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
