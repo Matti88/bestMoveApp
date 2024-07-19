564fe8ccad69b97e9cc5b739917a24af9ea12419
S4W4A <matteo.montanari@erstegroup.com>
2024-07-19 07:49:57 +0200

    special footer management

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,8 +3,13 @@ import { ModeToggle } from "../mode-toggle";
 import { PoiDrawer } from '@/components/ui/PoiDrawer';
 import  FormComponentFooter  from '@/components/ui/FormComponentFooter';
 import  FiltersComponentFooter  from '@/components/ui/FiltersComponentFooter';
+import { useLocation } from "react-router-dom";
 
 export function Footer() {
+
+    const location = useLocation();
+
+    if (location.pathname === "/mapwork") {
     return (
         <footer className="flex flex-col items-center justify-between gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row ml-20 mr-20" >
             <p className="text-center text-sm leading-loose text-muted-foreground md:text-left hidden md:block">Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.</p>
@@ -18,4 +23,15 @@ export function Footer() {
             </div>
         </footer>
     )
+}   else {
+    return (
+        <footer className="flex flex-col items-center justify-between gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row ml-20 mr-20" >
+            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left hidden md:block">Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.</p>
+            <div className="hidden md:block">
+                <ModeToggle />
+            </div>
+        </footer>
+    )
+}
+
 }
--- src/pages/Mapwork.tsx
+++ src/pages/Mapwork.tsx
@@ -1,12 +1,10 @@
 import MapComponent from '@/components/ui/MapComponent2';
 import FiltersComponent from '@/components/ui/FiltersComponent';
-import HouseListing from "@/components/ui/HouseListing";
-import houselistingStore from '@/store/houselistingStore';
 import FormComponent from "@/components/ui/FormComponent";
 import POIList from "@/components/ui/POIList"; 
 
 export default function Mapwork() {
-  const houses = houselistingStore((state) => state.houseListings);
+
 
   return (
     <>
@@ -23,16 +21,6 @@ export default function Mapwork() {
         <div className="absolute top-10 right-10 z-10 p-2 rounded max-w-md h-[80vh] w-[20vw] overflow-y-auto scaled-container hidden sm:block">
           <FiltersComponent />
           <br/>
-          {/* {houses.filter((house) => house.displayed).map((listing, index) => (
-            <HouseListing
-              key={index}
-              image={listing.image}
-              title={listing.title}
-              price={listing.price}
-              sqm={listing.sqm}
-              listingUrl={listing.link}
-            />
-          ))} */}
         </div>
       </div>
     </>
