0a7bfe08cced14c54f45db606ed6981fbe1c96d0
S4W4A <matteo.montanari@erstegroup.com>
2024-07-19 17:51:24 +0200

    loading conditions

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -6,32 +6,26 @@ import  FiltersComponentFooter  from '@/components/ui/FiltersComponentFooter';
 import { useLocation } from "react-router-dom";
 
 export function Footer() {
-
     const location = useLocation();
 
-    if (location.pathname === "/mapwork") {
-    return (
-        <footer className="flex flex-col items-center justify-between gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row ml-20 mr-20" >
-            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left hidden md:block">Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.</p>
-            <div className="hidden md:block">
-                <ModeToggle />
-            </div>
-            <div className="flex flex-row items-center gap-4 sm:hidden">
-                <FormComponentFooter />
-                <PoiDrawer />
-                <FiltersComponentFooter />
-            </div>
-        </footer>
-    )
-}   else {
-    return (
-        <footer className="flex flex-col items-center justify-between gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row ml-20 mr-20" >
-            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left hidden md:block">Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.</p>
-            <div className="hidden md:block">
-                <ModeToggle />
-            </div>
-        </footer>
-    )
-}
 
+    const isMapworkPage = (location.pathname === "/mapwork" || location.pathname === "/");
+
+    return (
+        <footer className="flex flex-col items-center justify-between gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row ml-20 mr-20">
+            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left hidden md:block">
+                Built by <a href={appConfig.author.url} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">{appConfig.author.name}</a>.
+            </p>
+            <div className="hidden md:block">
+                <ModeToggle />
+            </div>
+            {isMapworkPage && (
+                <div className="flex flex-row items-center gap-4 sm:hidden">
+                    <FormComponentFooter />
+                    <PoiDrawer />
+                    <FiltersComponentFooter />
+                </div>
+            )}
+        </footer>
+    );
 }
