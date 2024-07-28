8a5228d8bafa616a41e2bae42c6b5cc8be267b2f
S4W4A <matteo.montanari@erstegroup.com>
2024-07-28 21:15:22 +0200

    made the filter right

--- src/components/ui/FiltersComponent.tsx
+++ src/components/ui/FiltersComponent.tsx
@@ -1,136 +1,77 @@
-import React, { useState } from 'react';
-import { userSearchStore, POI } from '@/store/user-search';
-import { handleFormSubmit } from '@/api/api';
+import React from 'react';
+import { userSearchStore, ActiveFilters, POI } from '@/store/user-search';
+import ChipWithCheckbox from '@/components/ui/ChipArray';
+import { houselistingStore } from '@/store/houselistingStore';
 
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/shadcn/card";
 import { Label } from "@/components/ui/shadcn/label";
-import { Input } from "@/components/ui/shadcn/input"; 
+import { Input } from "@/components/ui/shadcn/input";
 import { Button } from "@/components/ui/shadcn/button";
-import { 
-  Select,
-  SelectTrigger,
-  SelectContent,
-  SelectItem
-} from "@/components/ui/shadcn/select";
 
-const FormComponent: React.FC = () => {
-  const { pois, addPOI } = userSearchStore();
+import { triggerNewSearch } from '@/api/api';
+import { checkPropertiesAndSelection } from '@/utils/utils';
 
-  const [formData, setFormData] = useState({
-    title: '',
-    location: '',
-    time: 5,
-    transportationMode: 'walk',
-  });
+const FiltersComponent: React.FC = () => {
+  const activeFilters = userSearchStore((state) => state.activeFilters);
+  const list_selectionPoi = activeFilters.selectedPoiIds;
+  const maximumPrice = activeFilters.maxPrice;
+  const minimumSqm = activeFilters.minSqm;
+  const updateHouseListings = houselistingStore((state) => state.updateHouseListings);
+  const updateActiveFilters = userSearchStore((state) => state.updateActiveFilters);
+  const toggleSelectedPoi = userSearchStore((state) => state.toggleSelectedPoi);
+  const pois = userSearchStore((state) => state.pois);
+  const { houseListings } = houselistingStore.getState();
 
-  const isDuplicatePOI = (newPOI: POI) => {
-    return pois.some(
-      (poi) =>
-        poi.title === newPOI.title ||
-        (poi.lon === newPOI.lon &&
-          poi.lat === newPOI.lat &&
-          poi.timeRange === newPOI.timeRange &&
-          poi.modeOfTransportation === newPOI.modeOfTransportation)
-    );
-  };
-
-  async function extractBoundingBox(multipolygon: any) {
-    let minLatitude = 90;
-    let maxLatitude = -90;
-    let minLongitude = 180;
-    let maxLongitude = -180;
-
-    for (const polygon of multipolygon.features[0].geometry.coordinates) {
-      for (const ring of polygon) {
-        for (const point of ring) {
-          const [longitude, latitude] = point;
-
-          minLatitude = Math.min(minLatitude, latitude);
-          maxLatitude = Math.max(maxLatitude, latitude);
-
-          minLongitude = Math.min(minLongitude, longitude);
-          maxLongitude = Math.max(maxLongitude, longitude);
-        }
-      }
-    }
-
-    return {
-      lon: {
-        min: minLongitude,
-        max: maxLongitude,
-      },
-      lat: {
-        min: minLatitude,
-        max: maxLatitude,
-      },
-    };
-  }
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    await handleFormSubmit(event, formData, pois, isDuplicatePOI, addPOI, extractBoundingBox);
+  const handleNewSearch = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await triggerNewSearch(activeFilters, houseListings, pois);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
+    updateActiveFilters({ [name]: value });
   };
 
   return (
     <Card className="w-full max-w-md">
       <CardHeader>
-        <CardTitle>Add Point of Interest Isochrone</CardTitle>
-        <CardDescription>Fill out the form below and generate isochrones around your Point of Interest.</CardDescription>
+        <CardTitle>Search Filters</CardTitle>
+        <CardDescription>Refine here your search by apply filter by price, area and Point of Interest</CardDescription>
       </CardHeader>
-    
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleNewSearch}>
         <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="name">POI Title</Label>
-            <Input name="title" type='text' onChange={handleChange} placeholder="Enter title" />
+          <div className="grid grid-cols-1">
+            <label className="block mb-2">POI Isochrones:</label>
+            <div className="flex mb-4">
+              <br/>
+              {list_selectionPoi?.map((chip) => (
+                <ChipWithCheckbox
+                  key={chip.id}
+                  id={chip.id}
+                  text={chip.text}
+                  isChecked={chip.isChecked}
+                  onToggle={() => toggleSelectedPoi(chip.id)}
+                />
+              ))}
+            </div>
           </div>
-
-          <div className="space-y-2">
-            <Label htmlFor="location">Address</Label>
-            <Input name="location" type="text" placeholder="Write here POI's address" onChange={handleChange} />
-          </div>
-
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-2">
-              <Label htmlFor="time">Time</Label>
-              <Select name="time" onValueChange={(value) => setFormData({ ...formData, time: Number(value) })}>
-                <SelectTrigger>{formData.time} minutes</SelectTrigger>
-                <SelectContent>
-                  {[5, 10, 15, 20, 25, 30, 45, 60, 70, 80, 90, 120].map((minutes) => (
-                    <SelectItem key={minutes} value={minutes.toString()}>{minutes} minutes</SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+              <Label htmlFor="maxPrice">Max Price</Label>
+              <Input type="number" placeholder="Max Price" name="maxPrice" onChange={handleChange} value={maximumPrice || ''} />
             </div>
-
             <div className="space-y-2">
-              <Label htmlFor="transportationMode">Trans Mode</Label>
-              <Select name="transportationMode" onValueChange={(value) => setFormData({ ...formData, transportationMode: value })}>
-                <SelectTrigger>{formData.transportationMode}</SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="walk">Walk</SelectItem>
-                  <SelectItem value="transit">Transit</SelectItem>
-                  <SelectItem value="drive">Car</SelectItem>
-                  <SelectItem value="bicycle">Bicycle</SelectItem>
-                </SelectContent>
-              </Select>
+              <label htmlFor="minSpace">Min Space</label>
+              <Input type="number" placeholder="Min Sq. Meters" name="minSqm" onChange={handleChange} value={minimumSqm || ''} />
             </div>
           </div>
         </CardContent>
-
         <CardFooter>
-          <Button type='submit' className="w-full">Add Poi</Button>
+          <Button type='submit' className="w-full">Search</Button>
         </CardFooter>
       </form>
     </Card>
   );
 };
 
-export default FormComponent;
+export default FiltersComponent;
