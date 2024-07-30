8234f36def34d86235a505efd2b838bf86a5c6d6
S4W4A <matteo.montanari@erstegroup.com>
2024-07-30 23:40:53 +0200

    fixed buttons

--- src/components/ui/TabsComponent.tsx
+++ src/components/ui/TabsComponent.tsx
@@ -4,6 +4,7 @@ import houselistingStore from '@/store/houselistingStore';
 import TableComponent from '@/components/ui/TableComponent'
 import HouseListing from "@/components/ui/HouseListing";
 import { ArrowUpIcon, ResetIcon } from "@/components/ui/icons/iconsCollection";
+import { GrPowerReset } from "react-icons/gr";
 
 
 
@@ -35,15 +36,19 @@ function TabsComponent() {
             <div className="flex items-center gap-20">
               <div className="ml-auto flex items-center gap-20">
                 <Button className="h-8 gap-1 hidden sm:block" size="sm" variant="outline" onClick={() => exportToSpreadsheet(houses.filter((house) => house.displayed), "Search_Export")}>
-                  <ArrowUpIcon className="h-3.5 w-3.5" />
+                <div className="flex flex-row">
+                  <ArrowUpIcon className="h-3.5 w-3.5 mr-3" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Export results of filtered houses</span>
+                </div>
                 </Button>
                 <Button className="h-8 gap-1 hidden sm:block" size="sm" variant="outline" onClick={() => updateHouseListings([])}>
-                  <ResetIcon className="h-3.5 w-3.5" />
+                <div className="flex flex-row">
+                  <GrPowerReset className="h-3.5 w-3.5 mr-3" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Reset Houses</span>
+                </div>
                 </Button>
                 <Button className="h-8 gap-1 sm:hidden" size="sm" variant="outline" onClick={() => updateHouseListings([])}>
-                  <ResetIcon className="h-3.5 w-3.5" />
+                  <GrPowerReset className="h-3.5 w-3.5" />
                 </Button>
               </div>
             </div>
