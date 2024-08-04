613fd411174dcfbb3056847da15eb2b8f6454f61
S4W4A <matteo.montanari@erstegroup.com>
2024-08-04 23:52:59 +0200

    nicer house description sheet

--- src/components/ui/PinInfoSmallScreen.tsx
+++ src/components/ui/PinInfoSmallScreen.tsx
@@ -15,25 +15,29 @@ interface PinInfoProps {
 const PinInfoSmallScreen: React.FC<PinInfoProps> = ({ thumbnail_image, title, price_num, sqm_num, linktoInsertion, isOpen, onClose }) => {
   if (!isOpen) return null;
 
+  if (title.length > 30) {
+    title = title.substring(0, 30) + '...';
+  }
+
   return (
     <Drawer open={isOpen} onOpenChange={onClose}>
-      <DrawerContent className="mx-auto w-full max-w-sm p-4 space-y-4">
-        <DrawerHeader>
+      <DrawerContent className="mx-auto w-full max-w-sm p-3 space-y-4">
+        <DrawerHeader className='p-2.5'>
           <DrawerTitle>{title}</DrawerTitle>
           <DrawerDescription></DrawerDescription>
         </DrawerHeader>
         <a href={linktoInsertion} target="_blank">
-        <div className='grid grid-cols-2 gap-4'>
-          <div>
-            <img className="max-w-[100px] max-h-[100px] mb-4 rounded" src={thumbnail_image} alt={`${title} Image`} />
+          <div className='grid grid-cols-3 gap-4'>
+            <div className="flex justify-center items-center col-span-2">
+              <img className="w-full h-full object-cover rounded" src={thumbnail_image} alt={`${title} Image`} />
+            </div>
+            <div className="flex flex-col justify-start">
+              <div className="text-sm font-semibold mb-2">Price: {price_num}</div>
+              <div className="text-sm font-semibold">&#x33A1;: {sqm_num}</div>
+            </div>
           </div>
-          <div>
-            <div className="text-sm font-semibold mb-2">Price: {price_num}</div>
-            <div className="text-sm font-semibold">&#x33A1;: {sqm_num}</div>
-          </div>
-        </div>
         </a>
-        <DrawerFooter>
+        <DrawerFooter className='p-2 pl-0 pr-0mt-1'>
           <DrawerClose asChild>
             <Button type='submit' className="w-full" onClick={onClose}>Close</Button>
           </DrawerClose>
